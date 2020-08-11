5f182c3067200c5024475dda13823ee380cd1c34
Steve Anderson <prototype909@msn.com>
2020-08-11 21:15:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function fetchBooks() {
-
+  const json = fetch('https://anapioficeandfire.com/api/books').then(resp => resp.json());
+    return json.then(json => renderBooks(json)
 }
 
 function renderBooks(books) {
